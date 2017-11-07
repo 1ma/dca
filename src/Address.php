b8bcb2a094dfd9add7082189d3dd25510f2d1ad4
Marcel Hernandez <e6990620@gmail.com>
2017-11-07 18:48:56 +0100

    added validation in the Address value object

--- src/Address.php
+++ src/Address.php
@@ -2,9 +2,34 @@
 
 namespace UMA\DCA;
 
+use LinusU\Bitcoin\AddressValidator;
+
 /**
  * Value object representing a valid bitcoin address.
  */
 class Address
 {
+    /**
+     * @var string
+     */
+    private $address;
+
+    private function __construct(string $candidate)
+    {
+        if (false === AddressValidator::isValid($candidate)) {
+            throw new \InvalidArgumentException("Invalid bitcoin address received. Got: $candidate");
+        }
+
+        $this->address = $candidate;
+    }
+
+    public function fromString(string $candidate): Address
+    {
+        return new self($candidate);
+    }
+
+    public function __toString(): string
+    {
+        return $this->address;
+    }
 }
